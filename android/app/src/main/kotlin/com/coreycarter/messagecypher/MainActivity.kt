f0bafa891a2d6f54c816a8fe7a63a0f5b33579db
Corey Carter <corey-carter@hotmail.com>
2020-03-17 11:01:10 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/coreycarter/messagecypher/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.coreycarter.messagecypher
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
